0306dff1080a9fdb09cd7371e5ada44de0cbbb01
Jovian Lim <kiriketsuki@gmail.com>
2025-02-10 13:54:46 +0800

    Create reads_lmao.js

---
+++ reads_lmao.js
@@ -0,0 +1,8 @@
+import xd from "lmao.js";
+function read_lmao() {
+ // imports xd from lmao.js and prints it out
+  console.log(xd);
+  if (xd === "123") {
+    console.log("everything works");
+  }
+};
